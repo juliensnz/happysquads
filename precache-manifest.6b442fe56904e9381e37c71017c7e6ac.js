289ad624b5bddfbb3c146ae1dc9b60dbbc49d23a
juliensnz <sumbobyboys@gmail.com>
2020-10-14 13:41:36 +0000

    Deploying to gh-pages from  @ dce5c185d9a3c6d2aa305c04ceff90bb9eeb8348 🚀

---
+++ precache-manifest.6b442fe56904e9381e37c71017c7e6ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8c043a8718f659f16af47561bf9823",
+    "url": "/happyfamilies/index.html"
+  },
+  {
+    "revision": "41a8134799eadcf84c85",
+    "url": "/happyfamilies/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0c8acc9cbfd9c2438ec1",
+    "url": "/happyfamilies/static/js/2.c9909f80.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/happyfamilies/static/js/2.c9909f80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a8134799eadcf84c85",
+    "url": "/happyfamilies/static/js/main.d73b0b27.chunk.js"
+  },
+  {
+    "revision": "6e92c64d7dbf1d9f6cf1",
+    "url": "/happyfamilies/static/js/runtime-main.37e68684.js"
+  }
+]);
